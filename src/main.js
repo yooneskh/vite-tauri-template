aaa2b27d38715a1a9574d27ba520934be6ed2e45
Yoones Khoshghadam <yooneskh@gmail.com>
2021-06-27 16:29:08 +0430

    feat -network- add ynetwork and handle multiple resolvers

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue';
 import vuetify from './plugins/vuetify';
+
 import App from './App.vue';
 
+import './plugins/configure-ynetwork';
+
+
 const app = createApp(App);
+
 app.use(vuetify);
 
 app.mount('#app');
---
+++ src/plugins/configure-ynetwork.js
@@ -0,0 +1,46 @@
+import { http } from '@tauri-apps/api';
+import YNetwork from 'ynetwork';
+
+async function tauriReuestResolver({ method, url, data, headers }) {
+
+  try {
+
+    const response = await http.fetch(url, {
+      url,
+      method: method.toUpperCase(),
+      body: data,
+      headers,
+      responseType: 2
+    });
+
+    let result = response.data;
+    let status = response.status ?? -1;
+    let responseHeaders = response.headers ?? {};
+
+    if (response.headers['content-type']?.includes('application/json')) {
+      result = JSON.parse(result);
+    }
+
+    return {
+      headers: responseHeaders,
+      status,
+      data: result
+    };
+
+  }
+  catch (error) {
+
+    return {
+      status: -1,
+      data: error.message,
+      headers: {},
+      error
+    };
+
+  }
+
+}
+
+if (import.meta.env.VITE_HTTP_HANDLER === 'NATIVE') {
+  YNetwork.requestRunner = tauriReuestResolver;
+}
