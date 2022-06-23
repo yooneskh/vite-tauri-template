1ffa39605a07e6d44d5ecd6290d1346e529c3418
Nabil <noufnouf703@gmail.com>
2022-06-23 04:55:49 +0200

    got the trends working

--- src/pages/home/home-page.vue
+++ src/pages/home/home-page.vue
@@ -1,23 +1,17 @@
 <script setup>
 
-import Counter from '../../components/counter.vue';
-import ColorChecker from '../../components/color-checker.vue';
-import Requester from '../../components/requester.vue';
+// import Counter from '../../components/counter.vue';
+// import ColorChecker from '../../components/color-checker.vue';
+// import Requester from '../../components/requester.vue';
+import Banner from '../../components/banner.vue';
+// import Requester from '../../components/requester.vue';
+import Trending from '../../components/trending.vue';
+
 
 </script>
 
 <template>
-  <v-container class="page">
-
-    <counter />
-
-    <color-checker class="mt-3" />
-
-    <requester class="mt-3" />
-
-    <v-btn block flat color="info" class="mt-3" to="/about-us">
-      Go To About Us
-    </v-btn>
-
-  </v-container>
+    <banner></banner>
+    <!-- <requester></requester> -->
+    <trending></trending>
 </template>
