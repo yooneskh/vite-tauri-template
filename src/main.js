c5263b0c0b03ee9de5e39bc069083e9dc7e9379a
Bilnaa <noufnouf703@gmail.com>
2022-07-14 19:57:09 +0200

    added the integration of the player

--- src/main.js
+++ src/main.js
@@ -5,10 +5,6 @@ import routes from './routes';
 
 import App from './app.vue';
 import './assets/main.css';
-import VueVideoPlayer from "vue-video-player";
-import "video.js/dist/video-js.css";
-import "vue-video-player/src/custom-theme.css";
-import "videojs-flash";
 
 import 'vue-global-api/ref';
 import 'vue-global-api/reactive';
@@ -20,7 +16,6 @@ import 'vue-global-api/watchEffect';
 
 const app = createApp(App);
 
-app.use(VueVideoPlayer);
 
 app.use(routes);
 app.use(vuetify);
--- src/pages/watch/watch-page.vue
+++ src/pages/watch/watch-page.vue
@@ -1,51 +1,14 @@
 <script setup>
-    import Banner from '../../components/banner.vue';
-    import { fetch ,Body} from '@tauri-apps/api/http';
-    import { onUnmounted, onUpdated } from 'vue';
-
-  
-    
-
+import Banner from '../../components/banner.vue';
+import { fetch ,Body} from '@tauri-apps/api/http';
+import {ref,nextTick, onMounted} from 'vue';
+import Artplayer from 'artplayer';
 </script>
 
-<template #fallback>
-  <banner></banner>
-  <select>
-    <option v-for="video in videos">
-      {{ video.quality }}
-    </option>
-  </select>
-  <!--load the video quality that's registred in the selection-->
-  <div class="player">
-  <video id="video-js" controls preload="auto" data-setup="{}" class="vjs-default-skin" width="640" height="480">
-  <source :src="getValueFromKey(videos,lang)" type="application/x-mpegURL"/>
-      <p class="vjs-no-js">
-        To view this video please enable JavaScript, and consider upgrading to a
-        web browser that
-        <a href="http://videojs.com/html5-video-support/" target="_blank">
-          supports HTML5 video
-        </a>
-      </p>
-  </video>
-  </div>
-  
-  <!-- <div class="player" v-for="video of videos">
-  {{video.quality}}
-    <video
-      id="vid1"
-      width="1280"
-      height="720"
-      class="video-js"
-      preload="false"
-      controls
-      data-setup="{}"
-    >
-      
-    </video>
-  </div> -->
-  
+<template>
+        <Banner></Banner>
+        <div id="player" style="width:510px;height: 350px;"></div>
 </template>
- 
 
 <script>
 export default {
@@ -53,34 +16,31 @@ export default {
     data() {
         return {
            videos: null,
-           lang: 'ja-JP fr-FR'
+           lang: 'ja-JP fr-FR',
+           subs: null,
+           id: window.location.href.split('/').pop()
         }
     },
     methods:{
       getQuality(keys) {
-        let quality = [];
-        for (var x = 0; x < keys.length; x++) {
-          let tKey = keys[x];
-          quality.push(tKey.quality);
-        }
-        return quality;
-      }
-      
+              let quality = [];
+              for (var x = 0; x < keys.length; x++) {
+                  let tKey = keys[x];
+                  quality.push(tKey.quality);
+              }
+              return quality;
+          },
     },
     created: async function() {
-    function Videos(quality,link,subs){
-        this.quality = quality;
-        this.link = link;
-        this.subs = subs;
-    }
-    function ModuleRequest(link, headers) {
-      this.link = link;
-      this.headers = headers;
-    }
-    function Subs(lang,link){
-        this.lang = lang;
-        this.link = link;
-    }
+    function Videos(quality, link) {
+              this.html = quality;
+              this.url = link;
+          }
+
+    function Subs(lang, link) {
+              this.html = lang;
+              this.url = link;
+          }
      
     async function getToken() {
       const url = 'https://kamyroll.herokuapp.com/auth/v1/token';
@@ -106,6 +66,7 @@ export default {
     }
     async function getVideos(id) {
         let videos = [];
+        let subtitles = [];
         var streams = '';
         var subs = '';
         const url = 'https://kamyroll.herokuapp.com/videos/v1/streams?channel_id=crunchyroll&id='+id+'&locale=en-US&type=adaptive_hls&format=vtt';
@@ -125,76 +86,115 @@ export default {
           for(streams of result.streams){
             var quality = streams.audio_locale + ' ' + streams.hardsub_locale;
             var link = streams.url;
-            link = new ModuleRequest(link, emptyHeaders);
-            
-            if(quality.split(' ')[1] == 'en-US' || quality.split(' ')[1] == 'fr-FR'){
-                
-                videos.push(new Videos(quality,link,null));
+            if(quality.split(' ')[1] == 'en-US' || quality.split(' ')[1] == 'fr-FR' ||streams.hardsub_locale==''){
+                videos.push(new Videos(quality,link));
                 continue;
-            } else if(!quality.split(' ')[1]) {
-                for(subs of result.subtitles){
-                    var lang = subs.locale;
-                    if(lang == 'en-US'){
-                      var subslink = subs.url;
-                      videos.push(new Videos(quality,link,new Subs(lang,subslink)));
-                    }
-                }
             }
           }
           return videos;
         } catch (e) {
-          ;
+          console.log(e);
         }
     }
-        var streams = await getVideos(window.location.href.split('/').pop());
-        this.videos = streams;
-    },
-    async mounted() {
-      var video = document.getElementById('video-js');
-      video.addEventListener('loadedmetadata', function() {
-        video.play();
-      });
-      // var select = document.querySelector('select');
-      // select.addEventListener('change', function() {
-      //   var quality = 'ja-JP en-US';
-      //   var video = document.querySelector('source').src;
-      //   video.src = getValueFromKey(this.videos,quality);
-      //   video.play();
-      // });
+    async function getSubs(id) {
+        let subtitles = [];
+        var streams = '';
+        var subs = '';
+        const url = 'https://kamyroll.herokuapp.com/videos/v1/streams?channel_id=crunchyroll&id='+id+'&locale=en-US&type=adaptive_hls&format=vtt';
+        var token = await getToken();
+        const headers = {
+          'Authorization': 'Bearer ' + token.access_token,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        };
+        try {
+          const response = await fetch(url, {
+            method: "GET",
+            headers: headers
+          })
+          ;
+          let result = response.data;
+          const emptyHeaders = null;
+          for(streams of result.streams){
+            var lang = streams.audio_locale + ' ' + streams.hardsub_locale;
+            var link = streams.url;
+            subtitles.push(new Subs(lang,link));
+          }
+          return subtitles;
+        } catch (e) {
+          console.log(e);
+        }
     }
+        var subs = await getSubs(this.id);
+        this.subs = subs;
+        var streams = await getVideos(this.id);
+        this.videos = streams;
+         var art = new Artplayer({
+        url: this.videos[0].url,
+        container: '#player',
+        volume: 0.5,
+        isLive: false,
+        muted: false,
+        autoplay: false,
+        pip: true,
+        autoSize: false,
+        autoMini: true,
+        setting: true,
+        loop: true,
+        flip: true,
+        playbackRate: true,
+        fullscreen: true,
+        fullscreenWeb: false,
+        subtitleOffset: true,
+        miniProgressBar: true,
+        mutex: true,
+        backdrop: true,
+        playsInline: true,
+        autoPlayback: true,
+        chromecast: true,
+        airplay: true,
+        theme: '#23ade5',
+        lang: navigator.language.toLowerCase(),
+        whitelist: ['*'],
+        moreVideoAttr: {
+            crossOrigin: 'anonymous',
+        },
+        quality: this.videos,
+        subtitle: subs[0],
+        settings:[{
+            width: 200,
+            html: 'Subtitle',
+            icon: '<img width="22" heigth="22" src="https://artplayer.org/assets/img/subtitle.svg">',
+            selector: [
+                {
+                    html: 'Display',
+                    tooltip: 'Show',
+                    switch: true,
+                    onSwitch: function (item) {
+                        item.tooltip = item.switch ? 'Hide' : 'Show';
+                        art.subtitle.show = !item.switch;
+                        return !item.switch;
+                    },
+                },
+                subs,
+            ],
+            onSelect: function (item) {
+                art.subtitle.switch(item.url, {
+                    name: item.html,
+                });
+                return item.html;
+            },
+        }],
+    })
+    },
 }
 </script>
-<!-- <template>
-  <div class="hello">
-    **<video
-      id="vid1"
-      width="500"
-      height="300"
-      class="video-js"
-      preload="auto"
-      poster="//vjs.zencdn.net/v/oceans.png"
-      controls
-      data-setup="{}"
-    >
-      <source src="https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8" type="application/x-mpegURL"/>
-      <p class="vjs-no-js">
-        To view this video please enable JavaScript, and consider upgrading to a
-        web browser that
-        <a href="http://videojs.com/html5-video-support/" target="_blank">
-          supports HTML5 video
-        </a>
-      </p>
-    </video>
-  </div>
-</template>
- -->
 
 <style scoped>
-select {
-  font-family: 'Roboto', sans-serif;
-  font-weight: 300;
-  color: #fff;
-  outline-color: white;
-  outline-width: 1px;
+#player{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    padding: 10px;
 }
 </style>
